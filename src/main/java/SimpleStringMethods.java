6f21ef5c391031303b24bfdc0bcf50b6681ac2ba
Brian Yang <yangbri1@msu.edu>
2024-12-04 02:23:59 +0000

    Java String methods

--- src/main/java/SimpleStringMethods.java
+++ src/main/java/SimpleStringMethods.java
@@ -14,7 +14,7 @@ public class SimpleStringMethods {
      * @return the length of string s.
      */
     public int getStringLength(String s){
-        return 0;
+        return s.length();
     }
 
     /**
@@ -24,7 +24,7 @@ public class SimpleStringMethods {
      * @return the character of String s that is located at position n.
      */
     public char getCharacterAtIndex(String s, int n){
-        return ' ';
+        return s.charAt(n);     // guess s[n] indexing doesn't work in Java like in JavaScript & C++
     }
 
     /**
@@ -36,7 +36,7 @@ public class SimpleStringMethods {
      * @return the index of the first occurence of c within s.
      */
     public int getIndexGivenCharacter(String s, char c){
-        return 0;
+        return s.indexOf(s);
     }
 
 }
